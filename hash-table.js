a436bfd5bedbfb574931ff71ead008465f4186c9
okawibawa <okaa.wibawa@gmail.com>
2024-07-09 21:00:21 +0800

    wip hash tables

--- hash-table.js
+++ hash-table.js
@@ -19,29 +19,34 @@ class HashTable {
   }
 
   _resize() {
-    console.log("Resizing hash table");
-
-    const newHashTable = this.data.length * 2;
+    const newHashTable = new Array(this.data.length * 2);
 
     this.data.forEach((bucket) => {
-      if (!bucket) {
-        console.log("Empty bucket");
-        return;
+      if (bucket) {
+        for (let i = 0; i < bucket.length; i++) {
+          const hash = this._hash(bucket[i][0]);
+
+          if (!newHashTable[hash]) {
+            newHashTable[hash] = [];
+          }
+
+          newHashTable[hash].push([bucket[i][0], bucket[i][1]]);
+        }
       }
     });
 
-    console.log(newHashTable);
+    this.data = newHashTable;
   }
 
   set(key, value) {
     this.numItems++;
     const hash = this._hash(key);
 
-    if (this.data[hash] === undefined) {
-      this.data[hash] = {};
+    if (!this.data[hash]) {
+      this.data[hash] = [];
     }
 
-    this.data[hash][key] = value;
+    this.data[hash].push([key, value]);
 
     if (this.numItems > this.data.length * 0.7) {
       this._resize();
@@ -49,21 +54,30 @@ class HashTable {
   }
 
   get(key) {
-    const hash = this._hash(key);
+    const bucket = this.data[this._hash(key)];
 
-    if (this.data[hash] === undefined) {
+    console.log(this.data);
+
+    console.log(bucket, this._hash(key));
+
+    if (!bucket) {
+      console.log("here");
       return undefined;
     }
 
-    return this.data[hash][key];
+    // return bucket.forEach((data) => {
+    //   if (data[0] === key) {
+    //     return data[1];
+    //   }
+    // });
   }
 }
 
 const newHashTable = new HashTable(5);
 
-newHashTable.set("name", "oka");
-newHashTable.set("language", "javascript");
-newHashTable.set("sex", "male");
-newHashTable.set("age", 26);
+newHashTable.set("gender", "male");
+newHashTable.set("fruits", "grapes");
+newHashTable.set("language", "japanese");
+newHashTable.set("profession", "programmer");
 
-console.log(newHashTable.getHashTable());
+console.log(newHashTable.get("gender"));
